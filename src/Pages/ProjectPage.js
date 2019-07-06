ad7ac22b4a7bf23294d256ab657ee96754c0649f
ehsan <ehsan.set1@gmail.com>
2019-07-06 13:00:57 -0700

    projectPage overall design finished

--- src/Pages/ProjectPage.js
+++ src/Pages/ProjectPage.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {connect } from 'react-redux'
-
+import FooterComponent from '../components/FooterComponent'
+import NavbarComponent from '../components/NavbarComponent'
+import ProjectPageLeftSidebarComponent from '../components/ProjectPageLeftSidebarComponent'
 
 
 const mapStateToProps = (state) =>{
@@ -33,19 +35,39 @@ class ProjectPage extends Component {
                 return project
         })[0];
 
-        console.log(project)
-
+        let keywordsJSX = project.keywords.map((keyword,index )=>{
+            return(<span key={keyword + index} class="badge badge-success fontsize09 p-1 mr-2">{keyword}</span>)
+        })
         return(
-            <div>
-                <img src={project.mainImage} width="200" height="200"/> 
-                <h1>{project.title}</h1> 
+
+      <div className="arlo_tm_content" >
+        <NavbarComponent />
+        <div className="row">
+          <div  className="primaryColor col-lg-3  col-xl-2 p-lg-0"  >
+            <ProjectPageLeftSidebarComponent preview={project.preview} source={project.source} />
+          </div>
+          <div className="arlo_tm_rightpart col-lg-9 col-xl-10 p-lg-0">
+            <div class="rightpart_inner h-100">
+           
+            <div className="text-justify m-5">
+                <h1 className="fontsize17" >{project.title}</h1> 
                 <br/>
-                <h3>{project.keywords.toString()}</h3> 
+                <div >{keywordsJSX}</div> 
                 <br/>
-                <h3>{project.content}</h3> 
+                <h3 className="fontsize10 lineHeight15" >{project.content}</h3> 
                 
             </div>
-         
+            
+            <FooterComponent />
+
+            
+          
+            </div>
+          </div>
+        
+
+        </div>
+      </div>       
          
          )
         
---
+++ src/components/ProjectPageLeftSidebarComponent.js
@@ -0,0 +1,27 @@
+import React, { Component }  from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenNib,faCode,faUserTie,faHome,faGraduationCap,faQuoteLeft,faEye} from '@fortawesome/free-solid-svg-icons'
+
+
+
+export default function ProjectPageLeftSidebarComponent({preview,source}){
+    return(
+              <div class="arlo_tm_leftpart_wrap d-none d-lg-block">
+              <div class="leftpart_inner">
+                  <div class="logo_wrap">
+                      <a href="#"><img src="img/logo/desktop-logo.png" alt="" /></a>
+                  </div>
+                  <div class="menu_list_wrap">
+                          <li><a className="btn btn-primary w-100 text-left" href="/"><FontAwesomeIcon className="mr-3" icon={faHome} />Home</a></li>
+                          <li><a className="btn btn-primary w-100 text-left" href={preview}><FontAwesomeIcon className="mr-3" icon={faEye} />View the project</a></li>
+                          <li><a className="btn btn-primary w-100 text-left" href={source}><FontAwesomeIcon className="mr-3" icon={faCode} />Source Code</a></li>
+
+                  </div>
+                  <div class="leftpart_bottom">
+                  </div>
+                  <a class="arlo_tm_resize" href="#"><i class="xcon-angle-left"></i></a>
+              </div>
+          </div>
+    )
+  }
+  
